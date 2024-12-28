431fb927a58f5c601b6a794f8fc5b3e543cdb625
alice3589 <ryutoiwa2929@gmail.com>
2024-12-28 13:52:40 +0900

    p8

--- Untitled-8.cpp
+++ Untitled-8.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void swap(int* Px, int* Py);
+void swap(int& x, int& y);
 
 int main()
 {
     int num1 = 5;
     int num2 = 10;
-
+    
     cout << "変数num1の値は" << num1 << "です\n";
     cout << "変数num2の値は" << num2 << "です\n";
     cout << "変数num1とnum2の値を交換します\n";
 
-    swap(&num1, &num2);
+    swap(num1, num2);
 
     cout << "変数num1の値は" << num1 << "です\n";
     cout << "変数num2の値は" << num2 << "です\n";
@@ -20,11 +20,11 @@ int main()
     return 0;
 }
 
-void swap(int* Px, int* Py)
+void swap(int& x, int& y)
 {
     int tmp;
-    
-    tmp = *Px;
-    *Px = *Py;
-    *Py = tmp;
+
+    tmp = x;
+    x = y;
+    y = tmp;
 }
