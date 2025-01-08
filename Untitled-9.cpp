9053c3a122ac9c1174b6d6eee1f294245b3aa240
alice3589 <ryutoiwa2929@gmail.com>
2025-01-08 16:19:42 +0900

    p9

--- Untitled-9.cpp
+++ Untitled-9.cpp
@@ -11,6 +11,16 @@ int main()
         cin >> test[i];
     }
 
+    for (int s = 0; s < num - 1; s++){
+        for (int t = s + 1; t < num; t++){
+            if(test[t] > test[s]){
+                int tmp = test[t];
+                test[t] = test[s];
+                test[s] = tmp;
+            }
+        }
+    }
+
     for (int j = 0; j < num; j++){
         cout << j+1 << "番目の人の点数は" << test[j] << "です。\n";
     } 
