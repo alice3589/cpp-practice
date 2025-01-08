08c31497ba739b982b4d4fea1d528fcb5476742b
alice3589 <ryutoiwa2929@gmail.com>
2025-01-08 18:39:52 +0900

    p9

--- Untitled-9.cpp
+++ Untitled-9.cpp
@@ -3,25 +3,12 @@ using namespace std;
 
 int main()
 {
-    const int sub = 2;
-    const int num = 5;
-    int test[sub][num];
+    int test[5] = {80, 60, 55, 22, 75};
 
-    test[0][0] = 80;
-    test[0][1] = 60;
-    test[0][2] = 22;
-    test[0][3] = 50;
-    test[0][4] = 75;
-    test[1][0] = 90;
-    test[1][1] = 55;
-    test[1][2] = 68;
-    test[1][3] = 72;
-    test[1][4] = 58;
-
-    for(int i = 0; i < num; i++){
-        cout << i + 1 << "番目の人の国語の点数は" << test[0][i] << "点です。\n";
-        cout << i + 1 << "番目の人の数学の点数は" << test[1][i] << "点です。\n";
-    }
+    cout << "test[0]の値は" << test[0] << "です。\n";
+    cout << "test[0]のアドレスは" << &test[0] << "です。\n";
+    cout << "testの値は" << test << "です。\n";
+    cout << "つまり*testの値は" << *test << "です。\n";
 
     return 0;
 }
