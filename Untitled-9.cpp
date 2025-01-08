efc15c75f989260d3fc8bd132617d600b5f4ac01
alice3589 <ryutoiwa2929@gmail.com>
2025-01-08 16:26:44 +0900

    p9

--- Untitled-9.cpp
+++ Untitled-9.cpp
@@ -3,27 +3,25 @@ using namespace std;
 
 int main()
 {
+    const int sub = 2;
     const int num = 5;
-    int test[num];
+    int test[sub][num];
 
-    cout << num << "人の点数を入力してください。\n";
-    for (int i = 0; i < num; i++){
-        cin >> test[i];
+    test[0][0] = 80;
+    test[0][1] = 60;
+    test[0][2] = 22;
+    test[0][3] = 50;
+    test[0][4] = 75;
+    test[1][0] = 90;
+    test[1][1] = 55;
+    test[1][2] = 68;
+    test[1][3] = 72;
+    test[1][4] = 58;
+
+    for(int i = 0; i < num; i++){
+        cout << i + 1 << "番目の人の国語の点数は" << test[0][i] << "点です。\n";
+        cout << i + 1 << "番目の人の数学の点数は" << test[1][i] << "点です。\n";
     }
-
-    for (int s = 0; s < num - 1; s++){
-        for (int t = s + 1; t < num; t++){
-            if(test[t] > test[s]){
-                int tmp = test[t];
-                test[t] = test[s];
-                test[s] = tmp;
-            }
-        }
-    }
-
-    for (int j = 0; j < num; j++){
-        cout << j+1 << "番目の人の点数は" << test[j] << "です。\n";
-    } 
 
     return 0;
 }
