665e67579c6548bdf83a593573c4222dc17f8eb8
alice3589 <ryutoiwa2929@gmail.com>
2025-02-22 19:39:07 +0900

    Add files via upload

---
+++ p11_1/p11_1/p11_1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+enum Week { SUN, MON, TUE, WED, THU, FRI, STA };
+
+int main()
+{
+    Week w;
+    w = SUN;
+
+    switch (w) {
+    case SUN: cout << "日曜日です。\n"; break;
+    case MON: cout << "月曜日です。\n"; break;
+    case TUE: cout << "火曜日です。\n"; break;
+    case WED: cout << "水曜日です。\n"; break;
+    case THU: cout << "木曜日です。\n"; break;
+    case FRI: cout << "金曜日です。\n"; break;
+    case STA: cout << "土曜日です。\n"; break;
+    default: cout << "何曜日か分かりません。\n";
+    }
+
+    return 0;
+}
