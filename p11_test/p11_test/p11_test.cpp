21b20e7d8b03a4a78f4d48d0eebc5cce8004e058
alice3589 <ryutoiwa2929@gmail.com>
2025-02-23 22:50:17 +0900

    Add files via upload

---
+++ p11_test/p11_test/p11_test.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Person {
+    int age;
+    double weight;
+    double height;
+};
+
+void aging(Person* p);
+
+int main()
+{
+    Person per1 = { 0, 0.0, 0.0 };
+
+    cout << "年齢を入力してください。\n";
+    cin >> per1.age;
+
+    cout << "体重を入力してください。\n";
+    cin >> per1.weight;
+
+    cout << "身長を入力してください。\n";
+    cin >> per1.height;
+
+    aging(&per1);
+
+    return 0;
+}
+
+void aging(Person* p)
+{
+    cout << "年齢" << p->age << "体重" << p->weight << "身長" << p->height << "です。\n";
+    cout << "1年経過しました。\n";
+    cout << "年齢" << p->age + 1 << "体重" << p->weight << "身長" << p->height << "です。\n";
+}
