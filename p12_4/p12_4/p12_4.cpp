f8dbc4a62b9c6a79812d47c16f5570b0ff8e5275
alice3589 <ryutoiwa2929@gmail.com>
2025-02-27 18:40:01 +0900

    Add files via upload

---
+++ p12_4/p12_4/p12_4.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+class Point {
+private:
+    int x;
+    int y;
+public:
+    int getX() { return x; }
+    int getY() { return y; }
+    void setX(int a);
+    void setY(int b);
+};
+
+void Point::setX(int a)
+{
+    if (a >= 0 && a <= 10) {
+        x = a;
+    }
+    else {
+        x = 0;
+    }
+}
+
+void Point::setY(int b)
+{
+    if (b >= 0 && b <= 10) {
+        y = b;
+    }
+    else {
+        y = 0;
+    }
+}
+
+int main()
+{
+    Point point1;
+
+    int x, y;
+
+    cout << "X座標を入力してください。\n";
+    cin >> x;
+
+    cout << "Y座標を入力してください。\n";
+    cin >> y;
+
+    point1.setX(x);
+    point1.setY(y);
+
+    cout << "座標は(" << point1.getX() << ',' << point1.getY() << ")です。\n";
+}
