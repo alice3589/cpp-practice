fd02eeb4ebbc1e930d45d75ee22f15333353d20c
alice3589 <ryutoiwa2929@gmail.com>
2025-03-06 19:00:43 +0900

    Add files via upload

--- p13_1/p13_1/p13_1.cpp
+++ p13_1/p13_1/p13_1.cpp
@@ -3,31 +3,38 @@ using namespace std;
 
 class Car {
 private:
-    int num;
-    double gas;
+	int num;
+	double gas;
 public:
-    Car();
-    void show();
+	Car();
+	Car(int n, double g);
+	void show();
 };
 
 Car::Car()
 {
-    num = 0;
-    gas = 0.0;
-    cout << "車を作成しました。\n";
+	num = 0;
+	gas = 0;
+	cout << "車を作成しました。\n";
+}
+
+Car::Car(int n, double g)
+{
+	num = n;
+	gas = g;
+	cout << "ナンバー" << num << "ガソリン量" << gas << "の車を作成しました。\n";
 }
 
 void Car::show()
 {
-    cout << "車のナンバーは" << num << "です。\n";
-    cout << "ガソリン量は" << gas << "です。\n";
+	cout << "車のナンバーは" << num << "です。\n";
+	cout << "ガソリン量は" << gas << "です。\n";
 }
 
 int main()
 {
-    Car car1;
+	Car car1;
+	Car car2(1234, 20.5);
 
-    car1.show();
-
-    return 0;
+	return 0;
 }
