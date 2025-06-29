421c6c9fd56844f4420785b6f95e860c0b8a1c95
alice3589 <ryutoiwa2929@gmail.com>
2025-06-29 23:53:06 +0900

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -2,27 +2,9 @@
 using namespace std;
 
 int main() {
-    string s, t;
-    cin >> s >> t;
+    int t, n, s;
 
-    set<char> allowed(t.begin(), t.end());
-
-    bool a = true;
-
-    for (int i = 1; i < s.length(); i++) {
-        if (isupper(s[i])) {
-            if (allowed.find(s[i - 1]) == allowed.end()) {
-                a = false;
-                break;
-            }
-        }
-    }
-
-    if (a == true) {
-        cout << "Yes" << endl;
-    } else {
-        cout << "No" << endl;
-    }
+    
 
     return 0;
 }
